1d9bb2254906a74bc8198140eac226bd4292ff67
piojeet <manjhipiojeet@gmail.com>
2025-07-15 17:31:26 +0530

    first commit

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://toksy-backend.onrender.com",
+    baseURL: "https://toksy-backend.onrender.com/api",
     withCredentials: true, //send cookies with the request
 })
